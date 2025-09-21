49df422f15ffeffc13a44dfdd1e5fa1d2c3ba5e9
kushwaha <anilkumar31413141@gmail.com>
2025-09-21 10:08:07 +0530

    This is 1st commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react'
+
+const App = () =>{
+
+const [user, setchangeUser] = useState("Anil Kumar")
+const [num, setNum] = useState(0)
+
+const changeUser = () =>{
+  console.log("Done")
+  setchangeUser("Rahul Sharma")
+}
+  return(
+    <div>
+      <h1>UserName is {user}</h1>
+      <button onClick={changeUser}>Change User</button>
+
+
+      <h3>Number is {num}</h3>
+      <button onClick={ ()=>setNum(num+1)}>Increment</button>
+      <button onClick={ ()=>setNum(num-1)}>Decrement</button>
+    </div>
+
+  )
+}
+
+export default App
